0386da82be97ec958167198b7165fd186096aa9d
markus-dev06 <s2510307057@fhooe.at>
2026-03-18 17:30:41 +0100

    code von felix

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.ev3devices import (
+    Motor,
+    TouchSensor,
+    ColorSensor,
+    InfraredSensor,
+    UltrasonicSensor,
+    GyroSensor,
+)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
@@ -16,20 +22,24 @@ from time import sleep
 MotorLinks = Motor(Port.B)
 MotorRechts = Motor(Port.D)
 
+base = DriveBase(MotorLinks, MotorRechts, 40, 140)
+
+base.straight(500)
+
 
 
 # Run the motor up to 500 degrees per second.
 # To a target angle of 90 degrees.
-MotorLinks.run_target(500, 90)
-
-# Create your objects here.
-ev3 = EV3Brick()
-
-
-# Write your program here.
-ev3.speaker.beep()
-sleep(1)
-ev3.speaker.beep()
-ev3.speaker.beep()
-ev3.speaker.beep()
-ev3.speaker.beep()
+# MotorLinks.run_target(500, 90)
+#
+# # Create your objects here.
+# ev3 = EV3Brick()
+#
+#
+# # Write your program here.
+# ev3.speaker.beep()
+# sleep(1)
+# ev3.speaker.beep()
+# ev3.speaker.beep()
+# ev3.speaker.beep()
+# ev3.speaker.beep()
